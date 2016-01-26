1e13523c3f70d564932580e87278420c8b1ea94e
Andrew Wreggelsworth <andwregg@umich.edu>
2016-01-25 21:46:17 -0500

    HW3 P4 and P5

--- list.c
+++ list.c
@@ -11,6 +11,15 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 	assert(head != NULL);
 	assert(new_element != NULL);
 
+	list_t* current_element;
+
+	current_element = *head;
+	while(current_element->next != NULL && current_element->next->index < new_element->index){
+		current_element = current_element->next;
+	}
+	new_element->next = current_element->next;
+	current_element->next = new_element;
+
 	return head;
 }
 
@@ -18,6 +27,19 @@ list_t* insert_sorted(list_t* head, list_t* new_element) {
 // the resulting list. You do not need to preserve the original list.
 list_t* reverse(list_t* head) {
 	assert(head != NULL);
+
+	list_t* previous_list;
+	list_t* current_list;
+	list_t* next_list;
+
+	previous_list = NULL;
+	current_list = *head;
+	while(current_list != NULL){
+		next_list = current_list->next;
+		current_list->next = previous_list;
+		previous_list = current_list;
+		current_list = next_list;
+	}
 
 	return head;
 }
